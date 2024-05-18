05fc4e6a16358c34ac3cfce3aaee125b050b0b76
Daniel Xavier <daniel.xds@hotmail.com>
2024-05-18 18:31:55 -0300

    inserindo mais de um registro

---
+++ inserindo_mais_de_1_registro.sql
@@ -0,0 +1,15 @@
+use virtualtech;
+
+select * from produto;
+
+insert into produto(nome, marca, quantidade, preco)
+values('Teclado', 'Dell', 25, 78.32),
+('Carregador de Notebook', 'Lenovo', 88, 38.80),
+('Micro-fone', 'Microsoft', 35, 110.18),
+('TV', 'LG', 9, 2151.32),
+('Microondas','Consul', 400, 1100.00),
+('Maquina de Lavar', 'Brastemp', 90, 2349.78),
+('Mouse', 'Dell', 90, 59.90),
+('Geladeira', 'Brastemp', 49, 3725.99),
+('Pen drive', 'Microsoft', 86, 29.90);
+
